e30d55098ca10afa7fa3f9f6563e3698ff74ec10
kiyo09 <60495508+kiyo09@users.noreply.github.com>
2020-04-19 01:52:22 +0900

    sessiontest

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)               # paramsハッシュで受け取ったemail値を小文字化し、email属性に渡してUserモデルから同じemailの値のUserを探して、user変数に代入
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user                   # user変数がデータベースに存在し、なおかつparamsハッシュで受け取ったpassword値と、userのemail値が同じ(パスワードとメールアドレスが同じ値であれば)true
+    ?user = User.find_by(email: params[:session][:email].downcase)
+    if ?user && ?user.authenticate(params[:session][:password])
+      log_in ?user
+      params[:session][:remember_me] == '1' ? remember(?user) : forget(?user)
+      redirect_to ?user
     else
       flash.now[:danger] = "Invalid email/password combination"                     
       render 'new'                                                              # newビューの出力
@@ -14,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out                                                                     # ログアウトする
+    log_out if logged_in?                                                                  # ログアウトする
     redirect_to root_url 
   end
 end
